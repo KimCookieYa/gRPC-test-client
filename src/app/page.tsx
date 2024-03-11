5d192e77e079528892d48c6050ba4dae88705c05
KimCookieYa <min49590@gmail.com>
2024-03-11 13:21:47 +0900

    feat: grpc 테스트 호출 로직

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,40 @@
+"use client";
+
+import {useEffect, useState} from "react";
+import {TruckerLocationGrpcServiceClient} from "../../grpc/HelloworldServiceClientPb";
+import {TruckerLocationRequest} from "../../grpc/helloworld_pb";
+
 export default function Home() {
+  const [rpcClient, setRpcClient] = useState<TruckerLocationGrpcServiceClient>();
+  const [rpcRequest, setRpcRequest] = useState<TruckerLocationRequest>();
+  const [message, setMessage] = useState<string>();
+
+  useEffect(() => {
+   const newRpcClient = new TruckerLocationGrpcServiceClient("http://localhost:8080", null, null);
+   setRpcClient(newRpcClient);
+
+   const newRpcRequest = new TruckerLocationRequest();
+    const temp = newRpcRequest.getOrderId();
+    console.log(temp)
+   setRpcRequest(newRpcRequest);
+  }, []);
+
+  const onButtonClick = async () => {
+    if (!rpcClient || !rpcRequest) {
+      return;
+    }
+
+    const response = await rpcClient.getTruckerLocations(rpcRequest);
+    console.log(response);
+  }
+
+
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       Hello, World! This is gRPC Test Page.
+      <button onClick={onButtonClick}>Send Request</button>
+      <p>{message}</p>
     </main>
   )
 }
