73319da42763544aaa376b80e44342cabdd22056
KimCookieYa <min49590@gmail.com>
2024-03-20 11:43:46 +0900

    feat: 토큰 발급 api 호출

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,13 @@ import * as grpcWeb from 'grpc-web';
 import {useEffect, useState} from "react";
 import {TruckerLocationServiceClient} from "../../grpc/helloworld_grpc_web_pb";
 import {TruckerLocationReply, TruckerLocationRequest} from "../../grpc/helloworld_pb";
+import axios from "axios";
 
 export default function Home() {
   const [rpcClient, setRpcClient] = useState<TruckerLocationServiceClient>();
   const [message, setMessage] = useState<string>();
+  const [orderId, setOrderId] = useState<number>(Number(process.env.NEXT_PUBLIC_ORDER_ID )|| 1)
+    const [jwtToken, setJwtToken] = useState<string>();
 
   useEffect(() => {
     if (!process.env.NEXT_PUBLIC_GRPC_URL) {
@@ -16,7 +19,6 @@ export default function Home() {
     }
    const newRpcClient = new TruckerLocationServiceClient(process.env.NEXT_PUBLIC_GRPC_URL, null, null);
    setRpcClient(newRpcClient);
-
   }, []);
 
   const onButtonClick = async () => {
@@ -24,13 +26,30 @@ export default function Home() {
       return;
     }
 
+    try {
+        const res = await axios.get('https://spring2024be.sendy.ngrok.io/generate/token', {
+            params: {
+                orderId: orderId
+            }
+        })
+        if (res?.data) {
+            console.log(res)
+            setJwtToken(res.data);
+        } else {
+            throw new Error('Failed to get JWT token');
+        }
+    } catch (e) {
+        console.error(e);
+        return;
+    }
+
     const newRpcRequest = new TruckerLocationRequest();
-    newRpcRequest.setOrderId(Number(process.env.NEXT_PUBLIC_ORDER_ID) || 1);
+    newRpcRequest.setOrderId(orderId);
     newRpcRequest.setManagerId(2);
 
     const call = rpcClient.getTruckerLocations(
         newRpcRequest, {
-            'Authorization': process.env.NEXT_PUBLIC_AUTHORIZATION
+            'Authorization': jwtToken
         } as grpcWeb.Metadata
     );
     call.on('status', (status: grpcWeb.Status) => {
@@ -58,10 +77,12 @@ export default function Home() {
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex flex-col min-h-screen flex-col items-center justify-between p-24">
       Hello, World! This is gRPC Test Page.
       <button onClick={onButtonClick}>Send Request</button>
       <p>{message}</p>
+        <p>order ID: {orderId}</p>
+        <p>jwt token: {jwtToken}</p>
     </main>
   )
 }
